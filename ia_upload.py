80a44762f10557d6268a79118b21aa780f0409ac
Shrinivasan <tshrinivasan@gmail.com>
2018-04-07 14:21:05 +0530

    Added translator details, fixed title

--- ia_upload.py
+++ ia_upload.py
@@ -8,6 +8,8 @@ book_info = yaml.load(open('book-info.yaml'))
 
 book_title_in_english = book_info['book_title_in_english']
 cover_image = book_info['cover_image']
+content = book_info["content"]
+
 timestamp = time.strftime('%Y-%m-%d-%H-%M-%S')
 
 ia_identifier = book_title_in_english + "-" + timestamp
@@ -18,7 +20,7 @@ tar = "tar czf " + book_title_in_english + ".tar.gz " + book_title_in_english + 
 os.system(tar)
 
 
-ia_upload = "ia upload " + ia_identifier + " -m collection:opensource -m mediatype:texts -m sponsor:FreeTamilEbooks -m language:ta " +  content_dir + book_title_in_english + ".epub " + content_dir + book_title_in_english + ".mobi " + content_dir + book_title_in_english + ".pdf " + content_dir + book_title_in_english + "_6_inch.pdf " +  content_dir + cover_image + " "  + book_title_in_english + ".tar.gz"
+ia_upload = "ia upload " + ia_identifier + " -m collection:opensource -m mediatype:texts -m sponsor:FreeTamilEbooks -m language:ta " +  content_dir + book_title_in_english + ".epub " + content_dir + book_title_in_english + ".mobi " + content_dir + book_title_in_english + ".pdf " + content_dir + book_title_in_english + "_6_inch.pdf " +  content_dir + cover_image + " "  + book_title_in_english + ".tar.gz" + " " + "book-info.yaml" + " " + content_dir + content
 
 print("Uploading to Internet Archive")
 
